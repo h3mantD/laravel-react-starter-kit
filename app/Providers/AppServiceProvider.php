c1884d4a082f464876a6e09ab9131a78c8490930
Hemant D <50235905+h3mantD@users.noreply.github.com>
2025-03-03 22:44:04 +0530

    refactor: rector changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,4 @@ final class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void {}
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void {}
 }
